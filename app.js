8d9696d93d70342c72e23eb4717661ee0c9c32f6
Simo <drlite23@gmail.com>
2025-06-07 17:38:58 +0100

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -104,33 +104,105 @@ function updateHoverStyle(color) {
 
 }
 
+let projects = [];
+
 class Project {
-  constructor(name,url){
+  constructor(name,url,info="simple info ."){
     this.name = name;
     this.url = url;
+    this.info = info;
   }
 }
-let dn = "https://arrachmohammed.dev/"
-dn='';
-let projects = [];
-let calculator = new Project("Calculator",dn+"calculator");
-let amazon = new Project("Amazon",dn+"Amazon%20project/amazon.html");
-let joke = new Project("Random Joke",dn+"joke");
-let movies = new Project("Movie info",dn+"movies");
-let PG = new Project("Phone Gadjet",dn+"Phone%20gadget");
-let rps = new Project("RPS",dn+"RPS%20project");
-let weatherBeta = new Project("Weather app",dn+"weather-beta");
-let youtube = new Project("Youtube",dn+"youtube%20project");
-projects.push(calculator,amazon,joke,movies,PG,rps,weatherBeta,youtube);
+
+
+
+
+
+let calculator = new Project("Calculator","calculator","A functional simple calculator .Helped me connect HTML, CSS, and JavaScript together in a functional way.");
+let amazon = new Project("Amazon","Amazon%20project","A clone of the Amazon website that helped me dive deeper into the DOM and JavaScript logic, using new concepts like OOP and asynchronous programming.");
+let joke = new Project("Random Jokes","joke","Simple web app for getting random jokes .My introduction to using APIs and working with external data.");
+let form = new Project("Simple form","simple_form"," A basic form project that taught me how HTML inputs and CSS styling work together.");
+let movies = new Project("Movie Search","movies","Simple web app for getting info about movies .Improved my understanding of APIs and dynamic content rendering.");
+let PG = new Project("Phone Gadjet","Phone%20gadget","My first website built for a real client a fully functional landing page.");
+let rps = new Project("RPS","RPS%20project","Rock Paper Scissors game .Helped me understand JavaScript logic through game development.");
+let weatherBeta = new Project("Weather app","weather-beta","Real time weather app .Took a deeper dive into APIs, callbacks, and promises.");
+let youtube = new Project("Youtube","youtube%20project","Youtube clone .Strengthened my CSS skills while recreating a familiar UI layout.");
+let todo = new Project("Todo App","todo%20project","A simple to-do list app that helped me practice DOM manipulation and event handling.")
+
+
+projects.push(form,rps,todo,youtube,calculator,amazon,joke,movies,weatherBeta,PG);
+
+
 projects.forEach((element)=>{
   let main = document.querySelector('main');
   let btn = document.createElement('button');
   let ankor = document.createElement('a');
+  let img = document.createElement('img');
   let info = document.createElement('p');
-  // info.innerText = "this info about the website";
   ankor.href = element.url;
   ankor.innerText = element.name;
-  ankor.appendChild(info);
+  img.src= element.url+"/index.png";
+  info.innerText = element.info;
+  btn.className='project';
   btn.appendChild(ankor);
+  btn.appendChild(img);
+  btn.appendChild(info);
   main.appendChild(btn);
 });
+
+
+let tech = [];
+
+class Technologies {
+  constructor(name,icon){
+    this.name = name;
+    this.icon = icon;
+  }
+}
+
+let html5 = new Technologies("HTML5",`fab fa-html5`);
+let css3 = new Technologies("CSS3",`fab fa-css3-alt`);
+let bootstrap = new Technologies("Bootstrap",`fab fa-bootstrap`);
+let js = new Technologies("JavaScript",`fab fa-js-square`);
+let react = new Technologies("React.js",`fab fa-react`);
+let nodeJS = new Technologies("Node.js",`fab fa-node-js`);
+let php = new Technologies("PHP",`fab fa-php`);
+let python = new Technologies("Python",`fab fa-python`);
+let sql = new Technologies("SQL",`fas fa-database`);
+
+tech.push(html5,css3,bootstrap,js,react,nodeJS,php,python,sql);
+let skills = document.querySelector('#skills');
+tech.forEach((element)=>{
+  let skill = document.createElement('button');
+  let icon = document.createElement('i');
+  let text = document.createElement('p');
+  text.innerText = element.name;
+  icon.className = element.icon+" fa-xl";
+  skill.className = "skill";
+  skill.appendChild(text);
+  skill.appendChild(icon);
+  skills.appendChild(skill);
+});
+
+let sw = window.innerWidth;
+const buttons = skills.children;
+const totalButtons = buttons.length;
+const buttonsToShow = 3;
+const buttonWidth = buttons[0].offsetWidth + 1;
+let currentIndex = 0;
+
+
+
+function slide() {
+  currentIndex += buttonsToShow;
+  if (currentIndex >= totalButtons) {
+    currentIndex = 0;
+  }
+  skills.style.transform = `translateX(-${currentIndex * buttonWidth}px)`;
+}
+
+
+if (sw>768){
+  skills.style.width = buttonWidth * totalButtons + "px";
+  setInterval(slide, 3000);
+};
